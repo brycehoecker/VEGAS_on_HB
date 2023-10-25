60d129503a90fc52b7707b738ec297e1c431dac8
brycehoecker <brycehoecker@gmail.com>
2023-10-25 09:04:11 -0700

    first local commit

---
+++ setup_VEGAS_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#TODO VEGAS INSTALLER SCRIPT checklist
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#1 Need to load conda module
+#2 In conda environment need to install VEGAS required programs cmake, gcc, etc...
+#3 In conda environment need to install ROOT (software from CERN)
+#4 Install veritas related software (vdb & vbf)
+#5 Actually install VEGAS software
+#6 ADD ADDITONAL STEPS HERE.....
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
